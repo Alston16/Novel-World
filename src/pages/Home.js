acebeab2a3bcb7f009b4a5600ef5fd1c1c60bf07
Alston Aranha <93255171+Alston16@users.noreply.github.com>
2022-11-13 20:24:38 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import NavBar from '../components/NavBar';
+import TabsBar from '../components/TabsBar';
+// import Carou from '../components/Carousel';
+
+export default function Home() {
+    return (
+        <div>
+            <NavBar />
+            <TabsBar />
+            {/* <Carou /> */}
+        </div>
+    );
+}
